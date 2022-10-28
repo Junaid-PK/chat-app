ab1e2dc5b2cd4074a7170a3e79178d8cd5bcca72
JUNAID-PK <junaidhussnain369@gmail.com>
2022-10-28 08:17:26 +0500

    Initialized new Project and added migrations

---
+++ database/seeders/ChatRoomSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ChatRoomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('chat_rooms')->insert([
+            'name' => 'General',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('chat_rooms')->insert([
+            'name' => 'Random',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('chat_rooms')->insert([
+            'name' => 'Laravel',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
